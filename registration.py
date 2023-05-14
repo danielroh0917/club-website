4b01050ed3c0b7f72530a3a3b8c1d6a8451b75d4
danielroh0917 <78741635+danielroh0917@users.noreply.github.com>
2023-05-14 21:02:39 +0900

    hw

--- registration.py
+++ registration.py
@@ -1,11 +1,15 @@
 import sqlite3 as sq # as <-- sqlite3를 sq로 짧게 바꿔주는 코드
+import bcrypt
 
 def register(username,password,re_password,birthday,gender,email,phone):
     conn = sq.connect("database/project.db") # conn = database 그자체(?)
     cur = conn.cursor() # cur = database에서 실행되는 모든것들을 책임지는 object(?)
     if password == re_password:
         try: # Primary Key인 username이 이미 존재할때 오류가 뜨지않고 메세지로 보여지게 만드는 코드 (오류가 뜨면 서버에 문제가 생기고 웹사이트가 작동이 안되기때문에 오류 메세지가 필요함)
-            conn.execute("INSERT INTO USERINFO(username,password,birthday,gender,email,phone) VALUES(?,?,?,?,?,?)",(username,password,birthday,gender,email,phone))
+            password = password.encode('utf-8')
+            salt = bcrypt.gensalt() # 한번더 꼬아서 암호화를 해주는 코드
+            hashed = bcrypt.hashpw(password, salt) # 암호화된 password와 salt를 합쳐주는 역할
+            conn.execute("INSERT INTO USERINFO(username,password,birthday,gender,email,phone) VALUES(?,?,?,?,?,?)",(username,hashed,birthday,gender,email,phone))
         except sq.IntegrityError:
             print("Already Exist")
         conn.commit()
@@ -22,10 +26,8 @@ def login(username,password):
     if user == None: #유저가 존재하지 않음
         return False
     #print(user) --> ('qwer',)
-    pw = user[0] # 'qwer' # user[0]에서 0은 user(Tuple)에서 indexr를 의미함
-    if password == pw:
-        return True
-    else: #유저는 존재하나 패스워드가 다름
-        return False
+    pw = user[0] # 'qwer' # user[0]에서 0은 user(Tuple)에서 index를 의미함
+    password_encoded = password.encode('utf-8') # 유저가 입력한 password를 0과 1로만 이루어진 컴퓨터 언어로 바꿔주는 코드
+    result = bcrypt.checkpw(password_encoded, pw) # pw(hased)를 password_encoded와 마찬가지로 컴퓨터 언어로 바꾼뒤에 userBy와 비교해주는 코드
 
 login("abcd","123456") #('qwer',)
